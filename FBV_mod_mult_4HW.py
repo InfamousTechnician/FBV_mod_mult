424ff658ff493160b8c8af7e6f1e1b7c07e3ca23
Fekete Balazs Valer <48908633+InfamousTechnician@users.noreply.github.com>
2023-08-02 04:01:33 +0200

    Add files via upload

---
+++ FBV_mod_mult_4HW.py
@@ -0,0 +1,45 @@
+# My own modulo multiplier algorithm based on number system conversion.
+# Disclaimer: idk, if this exists elsewhere, but it is a simple and
+# clearly hardware friendly algorithm. Both the comparator and the
+# subtractor work with constant 'm' on one input, and their other input can
+# be used alternately between 'x' and the 'accu'. y>0 can be a
+# fixed length counter in case of known cryptographic applications, so no
+# 3rd comparator is actually needed. While doubling and dividing by 2s is
+# just plain shifting left and right.
+
+def FBV_mod_mult_4HW(x,y,m): # A multi-digit might even make use of Kartasuba, idk.
+    accu = 0
+    while y > 0:
+        if y & 1 > 0:
+            accu += x
+            if accu > m:
+                accu -= m
+        x *= 2
+        if x > m: # this way even more HW prone... ;)
+            x -= m
+        y //= 2
+    return accu
+
+m = 16381
+x = 14532
+y = 3769
+
+print(x*y%m)
+print(FBV_mod_mult_4HW(x,y,m))
+print()
+
+m = 16369
+x = 6734
+y = 5305
+
+print(x*y%m)
+print(FBV_mod_mult_4HW(x,y,m))
+print()
+
+m = 16411
+x = 10356
+y = 15320
+
+print(x*y%m)
+print(FBV_mod_mult_4HW(x,y,m))
+print()
